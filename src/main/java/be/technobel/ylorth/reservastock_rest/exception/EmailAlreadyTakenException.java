8692bf415074b73da49acc969ccef65ba18c0457
YLorthPro <yann.lorthioir@bstorm.be>
2023-08-31 15:51:15 +0200

    ***

--- src/main/java/be/technobel/ylorth/reservastock_rest/exception/EmailAlreadyTakenException.java
+++ src/main/java/be/technobel/ylorth/reservastock_rest/exception/EmailAlreadyTakenException.java
@@ -3,6 +3,6 @@ package be.technobel.ylorth.reservastock_rest.exception;
 public class EmailAlreadyTakenException extends RuntimeException {
 
     public EmailAlreadyTakenException() {
-        super("the specified email is already taken");
+        super("the specified email is already taken!");
     }
 }
